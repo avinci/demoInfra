f9545cf0d5c1bc41b2a5e9635b6f9edf30ae3822
Avi Cavale <avi@shippable.com>
2016-10-06 15:09:00 -0700

    adding error handling for state

--- archiveState.sh
+++ archiveState.sh
@@ -3,17 +3,13 @@
 export REPO_RESOURCE_NAME="demo-infra-repo"
 
 arch_statefile() {
-  echo "Copying new state file"
-  echo "-----------------------------------"
-  local state_loc=/build/IN/$REPO_RESOURCE_NAME/gitRepo/terraform.tfstate
+  local state_loc="/build/IN/$REPO_RESOURCE_NAME/gitRepo/terraform.tfstate"
   if [ -f "$state_loc" ]; then
     echo "new state file exists, copying"
     echo "-----------------------------------"
     cp -vr /build/IN/$REPO_RESOURCE_NAME/gitRepo/terraform.tfstate /build/state/
   else
-    echo "Copying previous state file"
-    echo "-----------------------------------"
-    local previous_statefile_location=/build/previousState/terraform.tfstate
+    local previous_statefile_location="/build/previousState/terraform.tfstate"
     if [ -f "$previous_statefile_location" ]; then
       echo "previous state file exists, copying"
       echo "-----------------------------------"
